cdc2ff9ba5dfeaf2ba6f7da4f156f34b8517b677
sai ashish pinnamsetty <saiashish611999@gmail.com>
2026-03-06 08:18:38 +0530

    initial_commit

---
+++ src/main/java/com/ashcode/ashcode/AshcodeApplication.java
@@ -0,0 +1,13 @@
+package com.ashcode.ashcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashcode/ashcode/AshcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashcode.ashcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
